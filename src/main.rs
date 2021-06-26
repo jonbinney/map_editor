9c5c856f6266d865d19a27096e27f19f164d24cb
Jon Binney <jon.binney@gmail.com>
2021-06-25 21:34:47 -0400

    Create empty bevy plugin for map editor

---
+++ src/main.rs
@@ -0,0 +1,35 @@
+use bevy::prelude::*;
+
+fn setup_scene(
+    mut commands: Commands,
+    mut meshes: ResMut<Assets<Mesh>>,
+    mut materials: ResMut<Assets<StandardMaterial>>,
+) {
+    commands.spawn_bundle(PbrBundle {
+        mesh: meshes.add(Mesh::from(shape::Plane { size: 5.0 })),
+        material: materials.add(Color::rgb(0.3, 0.5, 0.3).into()),
+        ..Default::default()
+    });
+    commands.spawn_bundle(LightBundle {
+        transform: Transform::from_xyz(4.0, 8.0, 4.0),
+        ..Default::default()
+    });
+    commands.spawn_bundle(PerspectiveCameraBundle {
+        transform: Transform::from_xyz(-2.0, 2.5, 5.0).looking_at(Vec3::ZERO, Vec3::Y),
+        ..Default::default()
+    });
+}
+
+pub struct MapEditorPlugin;
+impl Plugin for MapEditorPlugin {
+    fn build(&self, app: &mut AppBuilder) {
+        app.add_startup_system(setup_scene.system());
+    }
+}
+
+fn main() {
+    App::build()
+        .add_plugins(DefaultPlugins)
+        .add_plugin(MapEditorPlugin)
+        .run();
+}
