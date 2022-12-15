6122088a68a70131be1c71601a09c927fb8d75e9
Jon Binney <jon.binney@gmail.com>
2022-12-14 19:00:21 -0500

    Switch to orbiting camera controls

--- src/camera_control.rs
+++ src/camera_control.rs
@@ -1,50 +1,39 @@
-use bevy::input::mouse::{MouseButton, MouseButtonInput, MouseMotion};
+use bevy::input::mouse::{MouseButton, MouseButtonInput, MouseMotion, MouseWheel};
 use bevy::input::ButtonState;
 use bevy::prelude::*;
 use bevy::render::camera::Camera;
-use std::f32::consts::FRAC_PI_2;
+
 pub struct CameraControlPlugin;
 impl Plugin for CameraControlPlugin {
     fn build(&self, app: &mut App) {
         app.init_resource::<CameraControlState>()
             .add_system(toggle_rotating)
-            .add_system(rotate_camera.after(toggle_rotating))
-            .add_system(update_camera_pose.after(rotate_camera));
+            .add_system(update_camera_pose.after(toggle_rotating));
     }
 }
 
 #[derive(Resource)]
 pub struct CameraControlState {
     pub rotating: bool,
-    pub position: Vec3,
-    pub yaw: f32,
-    pub pitch: f32,
+    pub azimuth: f32,
+    pub altitude: f32,
+    pub range: f32,
+    pub target: Vec3,
 }
 
 impl Default for CameraControlState {
     fn default() -> Self {
         Self {
             rotating: false,
-            position: Vec3 {
-                x: -2.0,
+            azimuth: 0.0,
+            altitude: std::f32::consts::PI / 4.0,
+            range: 2.5,
+            target: Vec3 {
+                x: 0.0,
                 y: 0.0,
-                z: 3.0,
+                z: 0.0,
             },
-            yaw: 0.0,
-            pitch: 1.1,
         }
-    }
-}
-
-pub fn update_camera_pose(
-    camera_control_state: ResMut<CameraControlState>,
-    mut camera_query: Query<(&Camera, &mut Transform)>,
-) {
-    for (_, mut transform) in camera_query.iter_mut() {
-        transform.translation = camera_control_state.position;
-        transform.rotation = Quat::from_rotation_z(camera_control_state.yaw)
-            * Quat::from_rotation_y(camera_control_state.pitch)
-            * Quat::from_mat3(&Mat3::from_cols(-Vec3::Y, Vec3::Z, -Vec3::X));
     }
 }
 
@@ -73,19 +62,59 @@ pub fn toggle_rotating(
     }
 }
 
-pub fn rotate_camera(
+pub fn update_camera_pose(
     _: Res<Windows>,
-    mut camera_control_state: ResMut<CameraControlState>,
+    mut state: ResMut<CameraControlState>,
+    mut camera_query: Query<(&Camera, &mut Transform)>,
     mut mouse_motion_events: EventReader<MouseMotion>,
+    mut mouse_scroll_events: EventReader<MouseWheel>,
 ) {
     for motion_event in mouse_motion_events.iter() {
-        if camera_control_state.rotating {
+        if state.rotating {
             debug!("{:?}", motion_event);
 
-            camera_control_state.yaw -= 0.01 * motion_event.delta.x;
-            camera_control_state.pitch -= 0.01 * motion_event.delta.y;
+            state.azimuth -= 0.01 * motion_event.delta.x;
+            state.altitude -= 0.01 * motion_event.delta.y;
         }
-
-        camera_control_state.pitch = camera_control_state.pitch.clamp(-FRAC_PI_2, FRAC_PI_2);
     }
+    state.altitude = state
+        .altitude
+        .clamp(std::f32::EPSILON, std::f32::consts::PI - std::f32::EPSILON);
+
+    for scroll_event in mouse_scroll_events.iter() {
+        state.range -= 0.4 * scroll_event.y;
+    }
+    state.range = state.range.clamp(std::f32::EPSILON, std::f32::INFINITY);
+
+    for (_, mut transform) in camera_query.iter_mut() {
+        *transform = compute_camera_transform(state.target, state.altitude, state.azimuth, state.range)
+    }
+}
+
+pub fn compute_camera_transform(
+    target: Vec3,
+    altitude: f32,
+    azimuth: f32,
+    range: f32,
+) -> Transform {
+    let position = Vec3 {
+        x: range * altitude.sin() * azimuth.cos(),
+        y: range * altitude.sin() * azimuth.sin(),
+        z: range * altitude.cos(),
+    };
+    Transform {
+        translation: position,
+        rotation: bevy_look_at(position, target),
+        ..default()
+    }
+}
+
+fn bevy_look_at(position: Vec3, target: Vec3) -> Quat {
+    // Camera forward is -z.
+    let camera_z = Vec3::normalize(position - target);
+    // Camera x is to the right.
+    let camera_x = Vec3::Z.cross(camera_z).normalize();
+    // Camera y is up.
+    let camera_y = camera_z.cross(camera_x);
+    Quat::from_mat3(&Mat3::from_cols(camera_x, camera_y, camera_z))
 }
